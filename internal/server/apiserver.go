bd7c6309fcdfa7476a28d762ed9f6c3a7e84c5dd
Artem <inctnce@icloud.com>
2021-01-25 15:54:56 +0300

    refactoring

--- internal/server/apiserver.go
+++ internal/server/apiserver.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 	"os"
 
+	// importing mysql driver for "database/sql" package
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/quizardapp/auth-api/internal/store/sqlstore"
 )
 
+// Start creates new db connection and starts the server on port declared in "config.env" file
 func Start() error {
 	db, err := newDB()
 	if err != nil {
@@ -19,8 +21,9 @@ func Start() error {
 	store := sqlstore.New(db)
 
 	server := newServer(store)
+	port := ":" + os.Getenv("PORT")
 
-	return http.ListenAndServe(":"+os.Getenv("PORT"), server)
+	return http.ListenAndServe(port, server)
 }
 
 func newDB() (*sql.DB, error) {
