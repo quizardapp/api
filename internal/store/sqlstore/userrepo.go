759420dec57e0dddf584568e90955296e276e42d
Artem <inctnce@icloud.com>
2021-01-25 15:55:35 +0300

    debug user creation method

--- internal/store/sqlstore/userrepo.go
+++ internal/store/sqlstore/userrepo.go
@@ -15,11 +15,23 @@ type UserRepo struct {
 // Create inserts new user into the database and returns error if something went wrong
 func (ur *UserRepo) Create(u *model.User) error {
 
+	if err := u.Validate(); err != nil {
+		return err
+	}
+
+	if err := u.GenerateToken("refresh"); err != nil {
+		return err
+	}
+
+	if err := u.GenerateToken("access"); err != nil {
+		return err
+	}
+
 	query := fmt.Sprintf(`
-				INSERT INTO users (id, firstname, lastname, email, password, creation_date) 
-				VALUES ('%s', '%s', '%s', '%s', '%s', '%s')
+		INSERT INTO users (id, firstname, lastname, email, password, creation_date, token) 
+		VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')
 						`,
-		u.ID, u.Firstname, u.Lastname, u.Email, u.Password, u.CreationDate)
+		u.ID, u.Firstname, u.Lastname, u.Email, u.Password, u.CreationDate, u.RefreshToken)
 	if _, err := ur.store.db.Exec(query); err != nil {
 		return err
 	}
@@ -34,7 +46,14 @@ func (ur *UserRepo) FindByEmail(email string) (*model.User, error) {
 
 	query := fmt.Sprintf("SELECT * FROM users WHERE email='%s'", email)
 	token := sql.NullString{}
-	if err := ur.store.db.QueryRow(query).Scan(&u.ID, &u.Firstname, &u.Lastname, &u.Email, &u.Password, &u.CreationDate, &token); err != nil {
+	if err := ur.store.db.QueryRow(query).Scan(
+		&u.ID,
+		&u.Firstname,
+		&u.Lastname,
+		&u.Email,
+		&u.Password,
+		&u.CreationDate,
+		&token); err != nil {
 		return nil, err
 	}
 	u.RefreshToken = token.String
@@ -49,7 +68,14 @@ func (ur *UserRepo) FindByID(id string) (*model.User, error) {
 
 	query := fmt.Sprintf("SELECT * FROM users WHERE id='%s'", id)
 	token := sql.NullString{}
-	if err := ur.store.db.QueryRow(query).Scan(&u.ID, &u.Firstname, &u.Lastname, &u.Email, &u.Password, &u.CreationDate, &token); err != nil {
+	if err := ur.store.db.QueryRow(query).Scan(
+		&u.ID,
+		&u.Firstname,
+		&u.Lastname,
+		&u.Email,
+		&u.Password,
+		&u.CreationDate,
+		&token); err != nil {
 		return nil, err
 	}
 	u.RefreshToken = token.String
