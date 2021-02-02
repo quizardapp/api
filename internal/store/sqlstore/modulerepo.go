b1b1b9c178fe1fcc0bdb52965da8e6c541f466ff
Artem <inctnce@icloud.com>
2021-02-02 17:11:36 +0300

    module creation

--- internal/store/sqlstore/modulerepo.go
+++ internal/store/sqlstore/modulerepo.go
@@ -1,23 +1,37 @@
 package sqlstore
 
-import "github.com/quizardapp/auth-api/internal/model"
+import (
+	"fmt"
+
+	"github.com/quizardapp/auth-api/internal/model"
+)
 
 type ModuleRepo struct {
 	store *SQLStore
 }
 
-func (cr *ModuleRepo) Create(m *model.Module) error {
+func (mr *ModuleRepo) Create(m *model.Module) error {
+
+	query := fmt.Sprintf(`
+	INSERT INTO modules (id, name, description, iduser, idcourse, creation_date) 
+	VALUES ('%s','%s','%s','%s','%s','%s')
+	`, m.ID, m.Name, m.Description, m.UserID, m.CourseID, m.CreationDate)
+
+	if _, err := mr.store.db.Exec(query); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (cr *ModuleRepo) Read() error {
+func (mr *ModuleRepo) Find(value string, field string) (*model.Module, error) {
+	return nil, nil
+}
+
+func (mr *ModuleRepo) Update(value string, field string, id string) error {
 	return nil
 }
 
-func (cr *ModuleRepo) Update() error {
-	return nil
-}
-
-func (cr *ModuleRepo) Delete() error {
+func (mr *ModuleRepo) Delete() error {
 	return nil
 }
