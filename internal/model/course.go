a8cf57fb5c2ce2d1cb9ebe50fd625625c229222a
Artem <inctnce@icloud.com>
2021-02-02 16:08:09 +0300

    refactoring

--- internal/model/course.go
+++ internal/model/course.go
@@ -1,14 +1,18 @@
 package model
 
-import "github.com/go-playground/validator/v10"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 // Course ...
 type Course struct {
-	ID           string `json:"id" validate:"required"`
-	Name         string `json:"name" validate:"required"`
-	Description  string `json:"description"`
-	UserID       string `json:"user_id" validate:"required"`
-	CreationDate string `json:"creation_date" validate:"required"`
+	ID           string    `json:"id" validate:"required"`
+	Name         string    `json:"name" validate:"required"`
+	Description  string    `json:"description"`
+	UserID       string    `json:"user_id" validate:"required"`
+	CreationDate time.Time `json:"creation_date" validate:"required"`
 }
 
 // Validate ...
