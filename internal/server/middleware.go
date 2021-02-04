ec3202bdfd39fe9a27dd02388a1a2cd326a3f55e
Artem <inctnce@icloud.com>
2021-02-04 12:40:36 +0300

    get access token from header

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -12,32 +9,18 @@ import (
 
 func (s *server) authenticate(next http.HandlerFunc) http.HandlerFunc {
 
-	type request struct {
-		ID          string `json:"id"`
-		AccessToken string `json:"access_token"`
-	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		req := &request{}
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			s.error(w, r, http.StatusInternalServerError, err)
-			return
-		}
-
-		r.Body.Close()
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
-		if err := json.NewDecoder(bytes.NewReader(body)).Decode(req); err != nil {
-			s.error(w, r, http.StatusInternalServerError, err)
+		accessToken := r.Header.Get("Authorization")
+		if accessToken == "" {
+			s.error(w, r, http.StatusUnauthorized, nil)
 			return
 		}
 
 		token := jwt.New(jwt.SigningMethodHS256)
 		claims := token.Claims.(jwt.MapClaims)
 
-		tkn, err := jwt.ParseWithClaims(req.AccessToken, claims, func(token *jwt.Token) (interface{}, error) {
+		tkn, err := jwt.ParseWithClaims(accessToken, claims, func(token *jwt.Token) (interface{}, error) {
 			return []byte(os.Getenv("TOKEN_SECRET")), nil
 		})
 
